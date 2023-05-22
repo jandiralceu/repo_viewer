3a20b0ba57a8befb1454db0c17d13b14dd01a47e
Jandir A. Cutabiala <jandir.dev@gmail.com>
2023-05-22 01:05:57 -0300

    feat: create auth application layer

---
+++ lib/auth/application/auth_notifier.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../domain/domain.dart';
+
+part 'auth_notifier.freezed.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  const factory AuthState.initial() = _Initial;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
+  const factory AuthState.authenticated() = _Authenticated;
+  const factory AuthState.failure(AuthFailure failure) = _Failure;
+}

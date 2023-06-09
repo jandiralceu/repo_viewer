abbd78353730dc8ec052de854d17b5bb12f0e6ee
Jandir A. Cutabiala <jandir.dev@gmail.com>
2023-06-08 23:47:09 -0300

    feat: building the sign in page

--- lib/auth/presentation/views/sign_in/sign_in_page.dart
+++ lib/auth/presentation/views/sign_in/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 @RoutePage()
 class SignInPage extends StatelessWidget {
@@ -7,6 +8,40 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Icon(MdiIcons.github, size: 150),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Welcome to\nRepo Viewer',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.green),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      child: Text('Sign in'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

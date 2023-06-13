2cc02012a596dc4ef1df4d74ee002a15bb9fb797
Jandir A. Cutabiala <jandir.dev@gmail.com>
2023-06-13 01:09:57 -0300

    feat: initial creation of the PaginatedReposListView

--- lib/github/repos/starred_repos/presentation/views/starred_repos/starred_repos_page.dart
+++ lib/github/repos/starred_repos/presentation/views/starred_repos/starred_repos_page.dart
@@ -1,24 +1,57 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:repo_viewer/github/core/shared/providers.dart';
 
 import '../../../../../../auth/auth.dart';
 
 @RoutePage()
-class StarredReposPage extends ConsumerWidget {
-  const StarredReposPage({super.key});
+class StarredReposPage extends ConsumerStatefulWidget {
+  const StarredReposPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ref.read(authNotifierProvider.notifier).signOut();
-          },
-          child: const Text('Sign out'),
-        ),
-      ),
+  StarredReposPageState createState() => StarredReposPageState();
+}
+
+class StarredReposPageState extends ConsumerState<StarredReposPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(
+      () => ref
+          .read(starredReposNotifierProvider.notifier)
+          .getNextStarredReposPage(),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Starred Repos'),
+        actions: [
+          IconButton(
+            icon: const Icon(MdiIcons.logoutVariant),
+            onPressed: () {
+              ref.read(authNotifierProvider.notifier).signOut();
+            },
+          )
+        ],
+      ),
+      body: PaginatedReposListView(),
+    );
+  }
+}
+
+class PaginatedReposListView extends StatelessWidget {
+  const PaginatedReposListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
   }
 }

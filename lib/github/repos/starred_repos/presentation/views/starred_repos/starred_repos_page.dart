74e9600c4d6235a46649880001fdceaff23b2688
Jandir A. Cutabiala <jandir.dev@gmail.com>
2023-06-26 17:14:14 -0300

    feat: adding parameters to the search bar

--- lib/github/repos/starred_repos/presentation/views/starred_repos/starred_repos_page.dart
+++ lib/github/repos/starred_repos/presentation/views/starred_repos/starred_repos_page.dart
@@ -1,10 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:repo_viewer/core/core.dart';
 
 import '../../../../../../auth/auth.dart';
+import '../../../../../../core/core.dart';
+import '../../../../../../search/presentation/presentation.dart';
 import '../../../../../core/core.dart';
 
 @RoutePage()
@@ -20,44 +20,36 @@ class _StarredReposPageState extends ConsumerState<StarredReposPage> {
   void initState() {
     super.initState();
 
-    Future.microtask(
-      () => ref
-          .read(starredReposNotifierProvider.notifier)
-          .getNextStarredReposPage(),
-    );
+    // Future.microtask(
+    //   () => ref
+    //       .read(starredReposNotifierProvider.notifier)
+    //       .getNextStarredReposPage(),
+    // );
+    ref.read(starredReposNotifierProvider.notifier).getNextStarredReposPage();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Starred Repos'),
-        actions: [
-          IconButton(
-            icon: const Icon(MdiIcons.logoutVariant),
-            onPressed: () {
-              ref.read(authNotifierProvider.notifier).signOut();
-            },
-          ),
-          IconButton(
-            icon: const Icon(MdiIcons.magnify),
-            onPressed: () {
-              AutoRouter.of(context).push(
-                SearchedReposRoute(searchTerm: 'flutter'),
-              );
-            },
-          )
-        ],
-      ),
-      body: PaginatedReposListView(
-        paginatedReposNotifierProvider: starredReposNotifierProvider,
-        getNextPage: (WidgetRef ref) {
-          ref
-              .read(starredReposNotifierProvider.notifier)
-              .getNextStarredReposPage();
+      body: AppSearchBar(
+        title: 'Starred repositories',
+        hint: 'Search all repositories',
+        onSignOut: () => ref.read(authNotifierProvider.notifier).signOut(),
+        onShouldNavigateToResultPage: (term) {
+          AutoRouter.of(context).push(
+            SearchedReposRoute(searchTerm: term),
+          );
         },
-        noResultMessage:
-            "That's about everything we could find in your starred repos right now.",
+        body: PaginatedReposListView(
+          paginatedReposNotifierProvider: starredReposNotifierProvider,
+          getNextPage: (WidgetRef ref) {
+            ref
+                .read(starredReposNotifierProvider.notifier)
+                .getNextStarredReposPage();
+          },
+          noResultMessage:
+              "That's about everything we could find in your starred repos right now.",
+        ),
       ),
     );
   }
--- lib/search/presentation/views/widgets/search_bar.dart
+++ lib/search/presentation/views/widgets/search_bar.dart
@@ -3,14 +3,30 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../shared/shared.dart';
 
-class SearchBar extends ConsumerStatefulWidget {
-  const SearchBar({super.key});
+typedef NavCallback = void Function(String term);
+typedef SignOutCallback = void Function();
+
+class AppSearchBar extends ConsumerStatefulWidget {
+  final String title;
+  final String hint;
+  final Widget body;
+  final NavCallback onShouldNavigateToResultPage;
+  final SignOutCallback onSignOut;
+
+  const AppSearchBar({
+    super.key,
+    required this.title,
+    required this.hint,
+    required this.body,
+    required this.onSignOut,
+    required this.onShouldNavigateToResultPage,
+  });
 
   @override
   _SearchBarState createState() => _SearchBarState();
 }
 
-class _SearchBarState extends ConsumerState<SearchBar> {
+class _SearchBarState extends ConsumerState<AppSearchBar> {
   @override
   void initState() {
     super.initState();
